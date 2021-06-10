7101f2a17691fd99105173a06ad68495ca7a1f60
nardo88 <nardo1988@mail.ru>
2021-06-10 17:53:30 +0300

    1

--- pitch-roulette/src/components/FormSignUp/FormSignUp.jsx
+++ pitch-roulette/src/components/FormSignUp/FormSignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 import '../Form/Form.css';
 
@@ -9,29 +9,29 @@ const FormSignUp = () => {
     const [password, setPasword] = useState('');
     const [passwordRepeat, setRasswordRepeat] = useState('');
 
+    // для редиректа
+    const [redirect, setRedirect] = useState(true);
+
     // отправка формы
-    const onSubmit = e => {
-        e.preventDefault()
-        // получаем данные полей
-        const formData = new FormData(e.target);
+    const onSubmitForm = e => {
+        e.preventDefault();
+        
 
-        // отправляем данные на серв
-        fetch('someApi.php', {
-            method: 'POST',
-            body: formData
-        })
-
-        e.target.reset();
+        // если все ок редиректим на следующий этап
+        setRedirect(false)
     }
 
     const changeInput = e => {
-        const { name, value } = e.target;
-        switch (name) {
+        const { id, value } = e.target;
+        switch (id) {
             case 'email':
                 setEmail(value);
                 break;
             case 'password':
                 setPasword(value);
+                break;
+            case 'passwordRepeat':
+                setRasswordRepeat(value);
                 break;
             default:
                 break;
@@ -41,38 +41,49 @@ const FormSignUp = () => {
 
     return (
         <>
+            {
+
+            redirect ? 
             <div className="registration">
                 <div className="container">
                     <div className="registration__wrapper">
 
                         <h2 className="registration__title">Sign up</h2>
-                        <form action="#!" className="form" onSubmit={onSubmit} autoComplete="off">
+                        <form action="#!" className="form" autoComplete="off" onSubmit={onSubmitForm}>
                             <div className="form__item">
                                 <label htmlFor="email" className="form__label">Business e-mail:</label>
-                                <input className="form__input" name="email" id="email" type="email" value={email} onChange={changeInput} placeholder="Your mail" />
+                                <input className="form__input" name="email" id="email" type="email" value={email} onChange={changeInput} placeholder="Your mail"  required/>
                             </div>
 
 
                             <div className="form__item">
                                 <label htmlFor="password" className="form__label">Password:</label>
-                                <input className="form__input" id="password" type="password" name="password" value={password} onChange={changeInput} placeholder="Your password" />
+                                <input className="form__input" id="password" type="password" name="password" value={password} onChange={changeInput} placeholder="Your password"  required/>
                             </div>
 
                             <div className="form__item">
                                 <label htmlFor="passwordRepeat" className="form__label">Repeat password:</label>
-                                <input className="form__input" id="passwordRepeat" type="password" value={passwordRepeat} onChange={changeInput} placeholder="Your password" />
+                                <input className="form__input" id="passwordRepeat" type="password" value={passwordRepeat} onChange={changeInput} placeholder="Your password" required />
                             </div>
 
 
                             <div className="form__btn-wrapper">
                                 <Link className="form__btn form__btn--two form__btn--transparent" to="/">Sign in</Link>
-                                <Link className="form__btn form__btn--two " to="/sign_up_step_2">Register</Link>
+                                <button className="form__btn form__btn--two ">Register</button>
                             </div>
                         </form>
 
                     </div>
                 </div>
             </div>
+
+            : 
+
+            <Redirect push to="/sign_up_step_2" />
+            
+            }
+
+
 
         </>
     )

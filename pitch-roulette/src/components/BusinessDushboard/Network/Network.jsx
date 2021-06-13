a69da08536a566add724faaac6ab48f12d4d34cd
Maxim Kuznecov <maksimkuznetsov@topverstka.ru>
2021-06-13 10:27:13 +0300

    1

--- pitch-roulette/src/components/BusinessDushboard/Network/Network.jsx
+++ pitch-roulette/src/components/BusinessDushboard/Network/Network.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Network = () => {
     return (
         <>
-            Network
+            <div className="network">
+                <div className="network__head">
+                    <h3 className="network__title">My network</h3>
+                    <span className="network__connections">{2148} connections</span>
+                </div>
+
+                <div className="network__content network-list">
+                    <div className="network-list__titles">
+
+                    </div>
+                    <ul className="network__list">
+                    </ul>
+                </div>
+            </div>
         </>
     ) 
 }

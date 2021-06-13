e0c0b29c4b76f3d84b8a480170ad294d125ec9f1
Maxim Kuznecov <maksimkuznetsov@topverstka.ru>
2021-06-13 09:56:30 +0300

    1

--- pitch-roulette/src/components/BusinessDushboard/GoLive/GoLive.jsx
+++ pitch-roulette/src/components/BusinessDushboard/GoLive/GoLive.jsx
@@ -7,11 +7,34 @@ import micro from '../../../img/control_icon/mic.svg';
 import up from '../../../img/control_icon/up.svg';
 import down from '../../../img/control_icon/down.svg';
 import addContact from '../../../img/control_icon/add_contact.svg';
-import partner from '../../../img/partner.jpg'
+import partner from '../../../img/partner.jpg';
+import btn1 from '../../../img/control_icon/btn1.svg';
+import stop from '../../../img/control_icon/stop.svg'
+import next from '../../../img/control_icon/next.svg'
+import fullscreen from '../../../img/control_icon/fullscreen.svg'
 
 const GoLive = () => {
 
-
+    const showReiting = num => {
+        const arrayStars = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < num) {
+                arrayStars.push(
+                    <span>
+                        <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#fff" d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
+                    </span>
+                
+                )
+            } else {
+                arrayStars.push(
+                    <span>
+                        <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#fff" d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="none" /></svg>
+                    </span>
+                )
+            }
+        }
+        return arrayStars;
+    }
 
     return (
         <>
@@ -29,21 +52,7 @@ const GoLive = () => {
                                 <div className="self__info">
                                     <div className="self__raiting">
                                         <div className="self__stars">
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="14" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m8.949 4.115.116.236.26.038 3.694.536-2.673 2.605-.188.184.044.259.631 3.679L7.53 9.915l-.233-.123-.232.123-3.304 1.737.63-3.679.045-.26-.188-.183-2.672-2.605 3.693-.536.26-.038.116-.236L7.297.768 8.95 4.115zm4.325.847zm-.07-.216h-.001zm-9.486 7.159z" stroke="#fff" /></svg>
-                                            </span>
+                                            {showReiting(4)}
                                         </div>
                                         <div className="self__raiting-value">4.0</div>
                                     </div>
@@ -79,23 +88,9 @@ const GoLive = () => {
                                 <div className="partner__info">
                                     <div className="partner__raiting">
                                         <div className="partner__stars">
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="13" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.717 4.722a.373.373 0 0 0-.301-.254l-3.948-.573L6.702.317a.373.373 0 0 0-.669 0L4.267 3.895.32 4.468a.373.373 0 0 0-.206.637l2.856 2.784-.674 3.933a.373.373 0 0 0 .541.393l3.532-1.857 3.53 1.857a.374.374 0 0 0 .542-.394L9.765 7.89l2.857-2.784a.373.373 0 0 0 .095-.383z" fill="#fff" /></svg>
-                                            </span>
-                                            <span>
-                                                <svg width="14" height="13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m8.949 4.115.116.236.26.038 3.694.536-2.673 2.605-.188.184.044.259.631 3.679L7.53 9.915l-.233-.123-.232.123-3.304 1.737.63-3.679.045-.26-.188-.183-2.672-2.605 3.693-.536.26-.038.116-.236L7.297.768 8.95 4.115zm4.325.847zm-.07-.216h-.001zm-9.486 7.159z" stroke="#fff" /></svg>
-                                            </span>
+                                            { showReiting(5) }
                                         </div>
-                                        <div className="partner__raiting-value">4.0</div>
+                                        <div className="partner__raiting-value">5.0</div>
                                     </div>
                                     <div className="partner__name">Jetex</div>
                                 </div>
@@ -118,10 +113,10 @@ const GoLive = () => {
                 
                 </div>
                 <div className="golive__control">
-                    <button className="golive__btn"></button>
-                    <button className="golive__btn"></button>
-                    <button className="golive__btn"></button>
-                    <button className="golive__btn"></button>
+                    <button className="golive__btn"><img src={btn1} alt="btn" /></button>
+                    <button className="golive__btn"><img src={stop} alt="stop" /></button>
+                    <button className="golive__btn"><img src={next} alt="next" /></button>
+                    <button className="golive__btn"><img src={fullscreen} alt="fullscreen" /></button>
                 </div>
 
             </div>
